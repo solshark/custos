deb96f13f61fbaab73ee72c58e5b0fff7864fb00
Mik B. Shvets <mik@wmt.dk>
2016-08-11 17:35:39 +0300

    autocommit

--- app/index.js
+++ app/index.js
@@ -14,7 +14,7 @@ export default class Client {
   constructor(props) {
     this.validate(props);
     this.API_DOMAIN = 'https://api.custos.cloud/';
-    this.mapper = new Mapper();
     this.VERSION = version;
+    this.mapper = new Mapper(this);
   }
 }
